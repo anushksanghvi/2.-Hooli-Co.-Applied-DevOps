90281b05652b945d166cac700d00b6b530b75db4
anushksanghvi <anushksanghvi@gmail.com>
2024-02-14 23:33:43 +0530

    Income Tax Calculator added

---
+++ IncomeTax_Calculator.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class IncomeTax_Calculator {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your income: Rs ");
+        double income = sc.nextDouble();
+        double tax;
+
+        if (income<500000){
+            tax = 0;
+            System.out.println("0% tax you will have to pay");
+        }
+ 
+        else{
+            tax = 0.3 * income;
+            System.out.println("30% tax you will pay");
+        }
+
+        System.out.println("Your tax is = Rs " + tax);
+    }
+}
+        
+    
+
+
+
